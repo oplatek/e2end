2c6604c052841fc30f94a71a70ae6faa184ac400
Ondrej Platek <ondrej.platek@seznam.cz>
2016-05-12 18:40:16 +0200

    full model compiles

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@
 Training script for dialogue state tracking.
 
 """
-import logging, uuid, random, os
+import logging, uuid, random, os, argparse
 import tensorflow as tf
 from e2end.utils import Config, git_info, setup_logging, Accumulator, elapsed_timer
 from e2end.training import EarlyStopper
@@ -12,7 +12,7 @@ from e2end.dataset.dstc2 import Dstc2, Dstc2DB
 import e2end.model
 
 
-def decode(m, dev, step, sess, i, t, dev_writer=None):
+def decode(m, dev, step, sess, i, t, dev_writer):
     with elapsed_timer() as valid_timer:
         acc_log = Accumulator(['mean', 'discard', 'activations', 'discard'])
         for di in range(dev):
@@ -33,55 +33,55 @@ def decode(m, dev, step, sess, i, t, dev_writer=None):
         dev_acc = agg_log_vals['acc']
         logger.info('Step %7d Dev Accuracy: %.4f', step, dev_acc)
     logger.info('Validation finished in %.2f s.', valid_timer())
+    return dev_acc
 
 
-def train(m, train, dev, config):
-    logger.info('Model %s compiled and loaded', m.__class__.__name__)
+def training(sess, m, train, dev, config, train_writer, dev_writer):
+    with elapsed_timer() as init_timer:
+        logger.info('Monitor progress by tensorboard:\ntensorboard --logdir "%s"\n', c.train_dir)
+        tf.initialize_all_variables().run(session=sess)
 
-    with tf.Session() as sess:
-        with elapsed_timer() as init_timer:
-            train_writer = tf.train.SummaryWriter(c.train_dir + '/train', sess.graph)
-            dev_writer = tf.train.SummaryWriter(c.train_dir + '/test', sess.graph)
-            logger.info('Monitor progress by tensorboard:\ntensorboard --logdir "%s"\n', c.train_dir)
-            tf.initialize_all_variables().run(session=sess)
+        stopper = EarlyStopper(c.nbest_models, c.not_change_limit, c.name)
+    logger.info('Graph initialized in %.2f s', init_timer())
 
-            stopper = EarlyStopper(c.nbest_models, c.not_change_limit, c.name)
-        logger.info('Graph initiliezed in %.2f s', init_timer())
+    try:
+        step = 0
+        for i in random.shuffle(range(len(train))):
+            for t in range(train.dial_lens[i]):
+                feed_dct = {m.dropout_keep_prob: c.dropout,
+                        m.new_turn: train.dialogs[i, t, :],
+                        m.turn_len: train.turn_lens[i, t],
+                        m.is_first_turn: 1 if t == 0 else 0,
+                        m.attention_mask: train.att_mask[i, t, :],
+                        m.turn_targets: train.turn_targets[i, t, :],
+                        m.turn_target_len: train._turn_target_lens[i, t]}
+                if step % c.train_sample_every == 0:
+                    _, step, *log_vals = sess.run(
+                            [m.train_op, m.global_step, m.loss, m.predictions, 
+                             m.att_time_db, m.summarize], feed_dict=feed_dct)
+                    logger.debug(m.log('train', train_writer, log_vals))
+                else:
+                    _, step = sess.run([t.train_op, t.global_step], feed_dict=feed_dct)
 
-        try:
-            step = 0
-            for i in random.shuffle(range(train)):
-                for t in range(train.dial_lens[i]):
-                    feed_dct = {m.dropout_keep_prob: c.dropout,
-                            m.new_turn: train.dialogs[i, t, :],
-                            m.turn_len: train.turn_lens[i, t],
-                            m.is_first_turn: 1 if t == 0 else 0,
-                            m.attention_mask: train.att_mask[i, t, :],
-                            m.turn_targets: train.turn_targets[i, t, :],
-                            m.turn_target_len: train._turn_target_lens[i, t]}
-                    if step % c.train_sample_every == 0:
-                        _, step, *log_vals = sess.run(
-                                [m.train_op, m.global_step, m.loss, m.predictions, 
-                                 m.att_time_db, m.summarize], feed_dict=feed_dct)
-                        logger.debug(m.log('train', train_writer, log_vals))
-                    else:
-                        _, step = sess.run([t.train_op, t.global_step], feed_dict=feed_dct)
-
-                    if step % c.validate_every == 0:
-                        decode(m, dev, step)
-                    if not stopper.save_and_check(dev_acc, step, sess):
-                        break
-        finally:
-            stopper.saver.save('%s-FINAL-STOP-%7d' % (stopper.saver_prefix, step))
-            logger.info('Training stopped after %7d steps and %7.2f epochs', step, step / len(train))
+                if step % c.validate_every == 0:
+                    dev_acc = decode(m, dev, step, sess, i, t, dev_writer)
+                if not stopper.save_and_check(dev_acc, step, sess):
+                    break
+    finally:
+        stopper.saver.save('%s-FINAL-STOP-%7d' % (stopper.saver_prefix, step))
+        logger.info('Training stopped after %7d steps and %7.2f epochs', step, step / len(train))
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument('--name', default='log/%(u)s/%(u)s' % {'u': uuid.uuid1()})
+    args = parser.parse_args()
+
     c = Config()  
-    c.name='log/%(u)s/%(u)s' % {'u': uuid.uuid1()}
+    c.name=args.name
     c.train_dir = c.name + '_traindir'
     c.filename = '%s.json' % c.name
-    c.vocab_file = '%s.vocab' % c.name
-    c.labels_file = '%s.labels' % c.name
+    c.words_vocab_file = '%s.vocab.words' % c.name
+    c.col_vocab_prefix = '%s.vocab.col.' % c.name
     c.log_name = '%s.log' % c.name
     c.seed=123
     c.train_file = './data/dstc2/data.dstc2.train.json'
@@ -90,6 +90,7 @@ if __name__ == "__main__":
     c.epochs = 2
     c.sys_usr_delim = ' SYS_USR_DELIM '
     c.learning_rate = 0.00005
+    c.max_gradient_norm = 5.0
     c.validate_every = 200
     c.train_sample_every = 200
     c.batch_size = 2
@@ -100,8 +101,15 @@ if __name__ == "__main__":
     c.feat_embed_size = 2
     c.nbest_models=3
     c.not_change_limit = 5  # FIXME Be sure that we compare models from different epochs
+    c.encoder_layers = 1
+    c.decoder_layers = 1
     c.sample_unk = 0
-    c.model_name='E2E_property_decoding' 
+    c.encoder_size = 100
+    c.word_embed_size = c.col_emb_size = 10
+    c.mlp_db_l1_size = 6 * c.col_emb_size + c.encoder_size
+    c.mlp_db_embed_l1_size = 6 * 10 * c.col_emb_size
+    c.model_name='E2E_property_decoding'
+    c.model_name='FastComp'
 
     os.makedirs(os.path.dirname(c.name), exist_ok=True)
     setup_logging(c.log_name)
@@ -113,22 +121,34 @@ if __name__ == "__main__":
     train = Dstc2(c.train_file, db, sample_unk=c.sample_unk, first_n=2 * c.batch_size)
     dev = Dstc2(c.dev_file, db,
             words_vocab=train.words_vocab,
-            labels_vocab=train.labels_vocab,
             max_turn_len=train.max_turn_len,
             max_dial_len=train.max_dial_len,
             max_target_len=train.max_target_len,
             first_n=2 * c.dev_batch_size)
 
     logger.info('Saving config and vocabularies')
+    c.col_vocab_sizes = [len(vocab) for vocab in db.col_vocabs]
+    c.max_turn_len = train.max_turn_len
+    c.max_target_len = train.max_turn_len
+    c.column_names = db.column_names
+    c.num_words = len(train.words_vocab)
+    c.num_rows = db.table.shape[0]
+    c.num_cols = db.table.shape[1]
     c.git_info = git_info()
     logger.info('Config\n\n: %s\n\n', c)
     logger.info('Saving helper files')
-    FIXME save all vocabularies
-    train.words_vocab.save(c.vocab_file)
+    train.words_vocab.save(c.words_vocab_file)
+    for vocab, name in zip(db.col_vocabs, db.column_names):
+        vocab.save(c.col_vocab_prefix + name)
     c.save(c.filename)
 
     m = getattr(e2end.model, c.model_name)(c)
+    logger.info('Model %s compiled and loaded', m.__class__.__name__)
 
-    train(m, train, dev, c)
+    with elapsed_timer() as sess_timer, tf.Session() as sess:
+        train_writer = tf.train.SummaryWriter(c.train_dir + '/train', sess.graph)
+        dev_writer = tf.train.SummaryWriter(c.train_dir + '/test', sess.graph)
+        logger.debug('Loading session took %.2f', sess_timer())
+        training(sess, m, train, dev, c, train_writer, dev_writer)
 
-    # TODO if decode only load the model parameter and test it
+        # TODO if decode only load the model parameter and test it
--- e2end/dataset/__init__.py
+++ e2end/dataset/__init__.py
@@ -7,23 +7,34 @@ import random
 
 __all__ = ['dstc.Dstc2', 'dstc.Dstc2DB']
 
+unk_label = 'UNK'
+
 
 class Vocabulary:
-    unk_label = 'UNK'
 
     @classmethod
     def load_json(cls, filename):
         d = json.load(open(filename, 'r'))
         return cls(**d)
 
-    def __init__(self, counts, max_items=5000, extra_words=None, unk=None):
+    def __init__(self, counts, max_items=5000, extra_words=None, unk=unk_label):
         self._counts = counts if isinstance(counts, Counter) else Counter(counts)
-        self.unk = unk or self.__class__.unk_label
-        self.extra_words = set(extra_words or [unk])
+        self.unk = unk = unk or []
+        extra_words = extra_words or []
+        self.extra_words = set(extra_words)
+        if unk:
+            self.extra_words.add(unk)
         self.max_items = max_items
         tmp = sorted(list(set(list(self.extra_words) + [w for w, _ in self._counts.most_common(max_items)])))
         self._w2int = dict(((w, i) for i, w in enumerate(tmp)))
         self._int2w = dict(((i, w) for i, w in enumerate(tmp)))
+        self._unk_id = self._w2int[unk] if unk else -666
+
+    def words(self):
+        return iter(self._w2int.keys())
+
+    def indexes(self):
+        return iter(self._int2w.keys())
 
     def get_i(self, w, unk_chance_smaller=0):
         if unk_chance_smaller:
@@ -31,21 +42,25 @@ class Vocabulary:
             if wc <= unk_chance_smaller and wc > random.uniform(0, unk_chance_smaller):
                 return self._w2int[self.unk]
             else:
-                return self._w2int.get(w, self._w2int[self.unk])
+                return self._w2int.get(w, self._unk_id)
         else:
-            return self._w2int.get(w, self._w2int[self.unk])
+            res = self._w2int.get(w, self._unk_id)
+            return res
 
     def get_w(self, index):
         return self._int2w[index]
 
     def __repr__(self):
+        return str(self.todict())
+
+    def todict(self):
         return {'counts': self._counts,
                 'max_items': self.max_items,
                 'unk': self.unk,
                 'extra_words': list(self.extra_words)}
 
     def save(self, filename):
-        json.dump(self.__repr__(), open(filename, 'w'), indent=4, sort_keys=True)
+        json.dump(self.todict(), open(filename, 'w'), indent=4, sort_keys=True)
 
     def __len__(self):
         return len(self._w2int)
