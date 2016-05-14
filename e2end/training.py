b47491f478f874b6f691f93da69f884d7e6b0251
Ondrej Platek <ondrej.platek@seznam.cz>
2016-05-14 16:56:36 +0200

    x

--- e2end/training.py
+++ e2end/training.py
@@ -17,7 +17,7 @@ class TrainingOps(object):
 
 
 class EarlyStopper(object):
-    '''Keeping track of n_best highest values in measure'''
+    '''Keeping track of n_best highest values in reward'''
     def __init__(self, track_n_best, not_change_limit, saver_prefix):
         self.n_best = track_n_best
         self.not_change_limit = not_change_limit
@@ -27,24 +27,24 @@ class EarlyStopper(object):
         self.saver_prefix = saver_prefix
 
     @property
-    def measures_steps_sessions(self):
+    def rewards_steps_sessions(self):
         '''Returns n_best results sorted from the highest to the smallest.'''
         return reversed([heapq.heappop(self._heap) for i in range(len(self._heap))])
 
-    def save_and_check(self, measure, step, sess):
+    def save_and_check(self, reward, step, sess):
         if len(self._heap) < self.n_best:
             self._not_improved = 0
-            heapq.heappush(self._heap, (measure, step, sess))
+            heapq.heappush(self._heap, (reward, step, sess))
         else:
-            last_measure = self._heap[0][0]
-            if last_measure < measure:
+            last_reward = self._heap[0][0]
+            if last_reward < reward:
                 heapq.heappop(self._heap)
-                heapq.heappush(self._heap, (measure, step, sess))
+                heapq.heappush(self._heap, (reward, step, sess))
                 self._not_improved = 0
             else:
-                logger.info('Not keeping measure %f from step %d', measure, step)
+                logger.info('Not keeping reward %f from step %d', reward, step)
                 self._not_improved += 1
         if self._not_improved == 0:  # we stored the model
-            path = self.saver.save(sess, '%s-%.4f-%7d' % (self.saver_prefix, measure, step))
-            logger.info('Sess: %f saved to %s', measure, path)
+            path = self.saver.save(sess, '%s-reward-%.4f-step-%7d' % (self.saver_prefix, reward, step))
+            logger.info('Sess: %f saved to %s', reward, path)
         return self._not_improved <= self.not_change_limit
