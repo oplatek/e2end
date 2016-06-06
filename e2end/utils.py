d21beb76cff0b95b9754a254ccc096021562c708
Ondrej Platek <ondrej.platek@seznam.cz>
2016-06-06 21:56:08 +0200

    not changed limit models 4

--- e2end/utils.py
+++ e2end/utils.py
@@ -181,7 +181,7 @@ def parse_input():
     ap.add_argument('--train_loss_every', type=int, default=100, help=' ')
     ap.add_argument('--validate_every', type=int, default=2264, help=' ')
     ap.add_argument('--nbest_models', type=int, default=3, help=' ')
-    ap.add_argument('--not_change_limit', type=int, default=10, help='Currently used both for RL and Xent updates')
+    ap.add_argument('--not_change_limit', type=int, default=4, help='Currently used both for RL and Xent updates')
     ap.add_argument('--sample_unk', type=int, default=0, help=' ')
     ap.add_argument('--dev_sample_every', type=int, default=10, help=' ')
     ap.add_argument('--batch_size', type=int, default=10, help=' ')
